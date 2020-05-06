0c9563813ea61f7d66301e69d5093f2a9e3d444f
Paras Agrawal <parasagrawal71@gmail.com>
2020-05-06 11:36:10 +0530

    feat: Add redux folder to contain actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./pages/App/App";
+import App from "./pages/app/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
