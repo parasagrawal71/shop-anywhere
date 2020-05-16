1f7edf78b6a96156747108b46ce395839e227fd9
Paras Agrawal <parasagrawal71@gmail.com>
2020-05-17 01:43:12 +0530

    feat: Add functionality to searchbox
    Add callbacks to search box(for suggestions and products) and use debouncing concept.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+// *** IMPORTS *** //
+import jsonServer from "apis/jsonServer";
 
 // *** IMPORT USER-DEFINED COMPONENTS HERE *** //
 import SearchBox from "components/searchBox/SearchBox";
@@ -8,12 +11,53 @@ import "./Header.css";
 import "assets/commonStyles/flex.css";
 
 const Header = () => {
+  // STATE VARIABLES
+  const [suggestionsData, setSuggestionsData] = useState([]);
+
+  // METHODS
+  const fetchSuggestions = async (query) => {
+    const response = await jsonServer(
+      "GET",
+      "suggestions",
+      {},
+      {
+        params: { query },
+        transformResponse: [
+          (data) =>
+            JSON.parse(data).filter((value) => value.name.startsWith(query)),
+        ],
+      }
+    );
+    setSuggestionsData(response);
+  };
+
+  const getProducts = async (query) => {
+    await jsonServer(
+      "GET",
+      "products",
+      {},
+      {
+        params: { query },
+        transformResponse: [
+          (data) =>
+            JSON.parse(data).filter((value) => value.name.startsWith(query)),
+        ],
+      }
+    );
+  };
+
   return (
     <div className="header flex-row-sb">
       <div className="header--left flex-row-v-cen">
         <div className="header__logo">Shop Anywhere</div>
         <div className="header__searchbox">
-          <SearchBox placeholder="Search for products" />
+          <SearchBox
+            placeholder="Search for products"
+            suggestionsCallback={fetchSuggestions}
+            suggestions={suggestionsData}
+            searchCallback={getProducts}
+            keyNames={{ id: "id", name: "name" }}
+          />
         </div>
       </div>
       <div className="header--right flex-row-v-cen">
--- src/components/searchBox/SearchBox.js
+++ src/components/searchBox/SearchBox.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// *** IMPORTS *** //
+import { debounce } from "utils/functions";
 
 // *** IMPORT STYLES HERE *** //
 import "./SearchBox.css";
@@ -7,17 +10,76 @@ import "assets/commonStyles/flex.css";
 // *** IMPORT ASSETS HERE *** //
 import searchIcon from "assets/svg/search-icon.svg";
 
+// VARIABLES
+let debouncedFunc;
+
 const SearchBox = (props) => {
-  const { placeholder } = props;
+  // PROPS
+  const {
+    placeholder,
+    suggestionsCallback,
+    suggestions,
+    searchCallback,
+    keyNames,
+  } = props;
+
+  // STATE VARIABLES
+  const [query, setQuery] = useState("");
+
+  // HOOKS
+  useEffect(() => {
+    debouncedFunc = debounce(suggestionsCallback, 400);
+  }, []);
+
+  // METHODS
+  const onInputChange = (e) => {
+    const q = e.target.value;
+    if (q !== "") {
+      setQuery(q);
+      debouncedFunc(q);
+    }
+  };
+
+  const onInputSubmit = () => {
+    if (query !== "") {
+      searchCallback(query);
+    }
+  };
+
+  // eslint-disable-next-line consistent-return
+  const onEnterKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      onInputSubmit();
+    } else {
+      return false;
+    }
+  };
+
+  // RENDER FUNCTIONS
+  const renderSuggestions = () => {
+    return suggestions.map((item) => (
+      <option key={item[keyNames.id]}>{item[keyNames.name]}</option>
+    ));
+  };
+
   return (
     <div className="searchbox flex-row">
-      <input placeholder={placeholder} className="searchbox__input" />
+      <input
+        type="search"
+        placeholder={placeholder}
+        className="searchbox__input"
+        // name="suggestions"
+        list="suggestions"
+        onChange={onInputChange}
+        onKeyDown={onEnterKeyPress}
+      />
+      <datalist id="suggestions">{renderSuggestions()}</datalist>
       <div
         role="button"
-        onClick={() => {}}
         className="searchbox__icon-wrapper flex-col-cen"
-        onKeyDown={() => {}}
         tabIndex="0"
+        onClick={onInputSubmit}
+        onKeyDown={onEnterKeyPress}
       >
         <img src={searchIcon} alt="Search" className="searchbox__icon" />
       </div>
