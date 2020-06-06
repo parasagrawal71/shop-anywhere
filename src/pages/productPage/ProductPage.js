41438bb7c52c35eef98c2e6d4bec3de8bca4b8da
Paras Agrawal <parasagrawal71@gmail.com>
2020-06-06 21:53:45 +0530

    feat: Add basic elements in product page

--- src/pages/productPage/ProductPage.js
+++ src/pages/productPage/ProductPage.js
@@ -9,9 +9,21 @@ import "./ProductPage.scss";
 
 const ProductPage = () => {
   return (
-    <div className="product">
+    <div className="product-page">
       <Header />
-      <div className="product__content" />
+      <div className="product-page__content flex-row">
+        <div className="product-page__content--left">
+          <div className="product-page__content-image flex-col-b-start-cen">
+            <img
+              src="https://m.media-amazon.com/images/I/81m+IKu7TUL._AC_UL320_.jpg"
+              alt="product-name"
+            />
+          </div>
+        </div>
+        <div className="product-page__content--right">
+          <div>DEtails</div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
