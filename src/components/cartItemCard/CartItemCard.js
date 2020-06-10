a8f55bda5fb5487e27864975d349554f55b96be0
Paras Agrawal <parasagrawal71@gmail.com>
2020-06-10 23:10:52 +0530

    feat: Add contents in cart page

--- src/components/cartItemCard/CartItemCard.js
+++ src/components/cartItemCard/CartItemCard.js
@@ -1,11 +1,67 @@
 import React from "react";
-// import PropTypes from "prop-types";
+
+// IMPORT USER-DEFINED COMPONENTS HERE //
+import Button from "subComponents/button/Button";
+
+// IMPORT STYLES HERE //
+import "./CartItemCard.scss";
+
+const brandName = "Peter England";
+const productTitle = "Men's Solid Slim fit T-Shirt";
+const actualPrice = 999;
+const offerPrice = 499;
 
 const CartItemCard = () => {
   return (
-    <div>
-      <div>CartItemCard</div>
-    </div>
+    <main className="cart-item">
+      <div>
+        <img
+          src="https://rukminim1.flixcart.com/flap/150/150/image/b616a7aa607d3be0.jpg?q=70"
+          alt="cart-item"
+        />
+      </div>
+      <section className="cart-item__details">
+        <div className="cart-item__brandName">{brandName}</div>
+        <div className="cart-item__productTitle">{productTitle}</div>
+        <div className="cart-item__prices">
+          <div className="cart-item__prices-offerPrice">
+            {`Rs ${offerPrice}`}
+          </div>
+          <div className="cart-item__prices-actualPrice">{actualPrice}</div>
+          <div className="cart-item__prices-discount">
+            {`(${((offerPrice / actualPrice) * 100).toFixed(0)} % Off)`}
+          </div>
+        </div>
+        <div className="cart-item__quantity">
+          <Button
+            btnText="-"
+            btnColor="white"
+            customContainerClass="cart-item__quantity-btnWrapper"
+            customBtnClass="cart-item__quantity-btn"
+          />
+          <div className="cart-item__quantity">3</div>
+          <Button
+            btnText="+"
+            btnColor="white"
+            customContainerClass="cart-item__quantity-btnWrapper"
+            customBtnClass="cart-item__quantity-btn"
+          />
+        </div>
+        <Button
+          btnText="Save for Later"
+          btnColor="white"
+          btnWidth="100px"
+          btnHeight="30px"
+          isShadow
+        />
+      </section>
+      <Button
+        btnText="X"
+        btnColor="white"
+        customContainerClass="cart-item__crossWrapper"
+        customBtnClass="cart-item__cross"
+      />
+    </main>
   );
 };
 
--- src/pages/cartPage/CartPage.js
+++ src/pages/cartPage/CartPage.js
@@ -2,6 +2,8 @@ import React from "react";
 
 // IMPORT USER-DEFINED COMPONENTS HERE //
 import Header from "components/header/Header";
+import CartItemCard from "components/cartItemCard/CartItemCard";
+import Button from "subComponents/button/Button";
 
 // IMPORT STYLES HERE //
 import "./CartPage.scss";
@@ -12,10 +14,41 @@ const CartPage = () => {
       <Header />
       <section className="cart__content">
         <section className="cart__content--left">
-          <div className="cart__content--left-header">My Cart (2)</div>
-          <div className="cart__content--left-item">Card Item</div>
+          <div className="cart__content--left-header">
+            <div className="cart__content--left-header-cart">My Cart (2)</div>
+            <div className="cart__content--left-header-later">
+              Save for Later (3)
+            </div>
+          </div>
+          <div className="cart__content--left-items">
+            <CartItemCard />
+            <CartItemCard />
+            <CartItemCard />
+          </div>
         </section>
-        <aside className="cart__content--right">ASide</aside>
+        <aside className="cart__content__aside">
+          <div className="cart__content__aside__header">Price Details</div>
+          <div className="cart__content__aside__price">
+            <div>Price</div>
+            <div>1299</div>
+          </div>
+          <div className="cart__content__aside__price">
+            <div>Delivery Fee</div>
+            <div>50</div>
+          </div>
+          <div className="cart__content__aside__price">
+            <div>Total</div>
+            <div>1349</div>
+          </div>
+          <Button
+            btnText="Place Order"
+            btnColor="white"
+            btnWidth="200px"
+            btnHeight="40px"
+            customContainerClass="cart__content__aside__placeOrder"
+            isShadow
+          />
+        </aside>
       </section>
     </div>
   );
